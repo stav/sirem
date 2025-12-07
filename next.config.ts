46d5f15b1b3c21aa9239d7c62c61dfbe3d3299d6
Steven Almeroth <steven.almeroth@gmail.com>
2025-12-07 15:38:00 -0500

    stricter referrer policy

--- next.config.ts
+++ next.config.ts
@@ -32,7 +32,7 @@ const nextConfig: NextConfig = {
           },
           {
             key: 'Referrer-Policy',
-            value: 'origin-when-cross-origin',
+            value: 'strict-origin-when-cross-origin',
           },
         ],
       },
